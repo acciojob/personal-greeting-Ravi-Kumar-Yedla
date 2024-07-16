96036f7494645456c38433f0b593fba1495697d2
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-07-16 12:04:11 +0530

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,56 @@
 
-// import React,{useState} from "react";
-// import './../styles/App.css';
+import React,{useState} from "react";
+import './../styles/App.css';
 
-// const App = () => {
-//   const[name,setName] = useState('');
-
-//   const handleChange =(event)=>{
-//      setName(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <p>Enter Your name:</p>
-//   <input 
-//     type="text"
-//     value={name}
-//     onChange={handleChange}
-//   />
-
-//   {name && <p>Hello {name}!</p>}
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-import React, { useState } from "react";
-import "./../styles/App.css";
 const App = () => {
-  const [text, setText] = useState("");
-  // var text = "ram";
-  const handleChange = (e) => {
-    console.log("Hello " + e.target.value + "!");
-    if (e.target.value === "") {
-      setText("");
-      return;
+  const[name,setName] = useState('');
+
+  const handleChange =(event)=>{
+
+    if(event.target.value === ""){
+      setName('')
+      return;gitgigg
     }
-    setText("Hello " + e.target.value + "!");
+     setName(event.target.value);
   };
+
   return (
     <div>
-      <input onChange={handleChange}></input>
-      <p>{text}</p>
+      <p>Enter Your name:</p>
+  <input 
+    type="text"
+    value={name}
+    onChange={handleChange}
+  />
+
+  {name && <p>Hello {name}!</p>}
     </div>
-  );
-};
-export default App;
+  )
+}
+
+export default App
+
+
+
+
+// import React, { useState } from "react";
+// import "./../styles/App.css";
+// const App = () => {
+//   const [text, setText] = useState("");
+//   // var text = "ram";
+//   const handleChange = (e) => {
+//     console.log("Hello " + e.target.value + "!");
+//     if (e.target.value === "") {
+//       setText("");
+//       return;
+//     }
+//     setText("Hello " + e.target.value + "!");
+//   };
+//   return (
+//     <div>
+//       <input onChange={handleChange}></input>
+//       <p>{text}</p>
+//     </div>
+//   );
+// };
+// export default App;
