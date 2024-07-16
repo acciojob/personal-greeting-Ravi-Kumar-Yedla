fd31c518d24355edc94afd710ac4395f48852f68
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-07-16 11:58:37 +0530

    finnaly

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,51 @@
 
-import React,{useState} from "react";
-import './../styles/App.css';
+// import React,{useState} from "react";
+// import './../styles/App.css';
 
+// const App = () => {
+//   const[name,setName] = useState('');
+
+//   const handleChange =(event)=>{
+//      setName(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <p>Enter Your name:</p>
+//   <input 
+//     type="text"
+//     value={name}
+//     onChange={handleChange}
+//   />
+
+//   {name && <p>Hello {name}!</p>}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+import React, { useState } from "react";
+import "./../styles/App.css";
 const App = () => {
-  const[name,setName] = useState('');
-
-  const handleChange =(event)=>{
-     setName(event.target.value);
+  const [text, setText] = useState("");
+  // var text = "ram";
+  const handleChange = (e) => {
+    console.log("Hello " + e.target.value + "!");
+    if (e.target.value === "") {
+      setText("");
+      return;
+    }
+    setText("Hello " + e.target.value + "!");
   };
-
   return (
     <div>
-      <p>Enter Your name:</p>
-  <input 
-    type="text"
-    value={name}
-    onChange={handleChange}
-  />
-
-  {name && <p>Hello {name}!</p>}
+      <input onChange={handleChange}></input>
+      <p>{text}</p>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
