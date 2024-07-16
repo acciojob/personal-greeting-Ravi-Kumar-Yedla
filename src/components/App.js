7eddbf1162233c8f1e567338d0d3ea1b191dce24
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-07-16 12:12:29 +0530

    finnaly done1

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,56 @@
 
-import React,{useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  const[name,setName] = useState('');
-
-  const handleChange =(event)=>{
-
-    if(event.target.value === ""){
-      setName('')
-      return;gitgigg
+  const [text, setText] = useState("");
+  
+  const handleChange = (e) => {
+    // console.log("Hello " + e.target.value + "!");
+    if (e.target.value === "") {
+      setText("");
+      return;
     }
-     setName(event.target.value);
+    setText("Hello " + e.target.value + "!");
   };
-
   return (
     <div>
-      <p>Enter Your name:</p>
-  <input 
-    type="text"
-    value={name}
-    onChange={handleChange}
-  />
-
-  {name && <p>Hello {name}!</p>}
+      <input onChange={handleChange}></input>
+      <p>{text}</p>
     </div>
-  )
-}
-
-export default App
-
+  );
+};
+export default App;
 
 
 
-// import React, { useState } from "react";
-// import "./../styles/App.css";
+
+// import React,{useState} from "react";
+// import './../styles/App.css';
+
 // const App = () => {
-//   const [text, setText] = useState("");
-//   // var text = "ram";
-//   const handleChange = (e) => {
-//     console.log("Hello " + e.target.value + "!");
-//     if (e.target.value === "") {
-//       setText("");
+//   const[name,setName] = useState('');
+
+//   const handleChange =(event)=>{
+
+//     if(event.target.value === ""){
+//       setName('')
 //       return;
 //     }
-//     setText("Hello " + e.target.value + "!");
+//      setName(event.target.value);
 //   };
+
 //   return (
 //     <div>
-//       <input onChange={handleChange}></input>
-//       <p>{text}</p>
+//   <input 
+//     type="text"
+//     value={name}
+//     onChange={handleChange}
+//   />
+
+//   {name && <p>Hello {name}!</p>}
 //     </div>
-//   );
-// };
-// export default App;
+//   )
+// }
+
+// export default App
